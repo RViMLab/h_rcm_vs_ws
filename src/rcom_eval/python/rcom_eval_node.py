0d2b8849dc548109755ffab8d7bc25d0d41b9a0e
mhubii <m.huber_1994@hotmail.de>
2021-02-11 17:38:17 +0000

    updated namespace of eval node

--- src/rcom_eval/python/rcom_eval_node.py
+++ src/rcom_eval/python/rcom_eval_node.py
@@ -38,7 +38,7 @@ def rCoMStateCB(msg: rcom):
 mean_pairwise_distance_sub = rospy.Subscriber("visual_servo/mean_pairwise_distance", Float64, meanPairwiseDistanceCB)
 twist_sub = rospy.Subscriber("visual_servo/twist", Twist, twistCB)
 h_rcom_vs_feedback_sub = rospy.Subscriber("h_rcom_vs/RCoM_ActionServer/feedback", rcomActionFeedback, hRCoMVSFeedbackCB)
-rcom_state_sub = rospy.Subscriber("RCoM_ActionServer/state", rcom, rCoMStateCB)
+rcom_state_sub = rospy.Subscriber("h_rcom_vs/RCoM_ActionServer/state", rcom, rCoMStateCB)
 
 # experiments
 #   - visual servo
