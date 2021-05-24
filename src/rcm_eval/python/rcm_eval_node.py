37e9094c12006909c28b6417a76ec548afe65b58
mhubii <m.huber_1994@hotmail.de>
2021-05-24 15:46:30 +0100

    added prelim metrics

--- src/rcm_eval/python/rcm_eval_node.py
+++ src/rcm_eval/python/rcm_eval_node.py
@@ -6,17 +6,18 @@ import rospy
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 from rcm_msgs.msg import rcm, rcmActionFeedback
+from h_vs.msg import pairwise_distance
 
 # dataframes to hold measurements
-mean_pairwise_distance_df = pd.DataFrame(columns=["mean_pairwise_distance"])
+pairwise_distance_df = pd.DataFrame(columns=["pairwise_distance"])
 twist_df = pd.DataFrame(columns=["Twist"])
 h_rcm_vs_feedback_df = pd.DataFrame(columns=["rcmFeedback"])
 rcm_state_df = pd.DataFrame(columns=["rcm"])
 
 # callbacks
-def meanPairwiseDistanceCB(msg: Float64):
-    global mean_pairwise_distance_df
-    mean_pairwise_distance_df = mean_pairwise_distance_df.append({"mean_pairwise_distance": msg}, ignore_index=True)
+def meanPairwiseDistanceCB(msg: pairwise_distance):
+    global pairwise_distance_df
+    pairwise_distance_df = pairwise_distance_df.append({"pairwise_distance": msg}, ignore_index=True)
 
 def twistCB(msg: Twist):
     global twist_df
@@ -31,11 +32,11 @@ def rCoMStateCB(msg: rcm):
     rcm_state_df = rcm_state_df.append({"rcm": msg}, ignore_index=True)
 
 # subscribe to
-#   - /lbr/visual_servo/mean_pairwise_distance   measure visual error minimization
+#   - /lbr/visual_servo/pairwise_distance   measure visual error minimization
 #   - /lbr/visual_servo/twist                    measure error minimization
 #   - /lbr/h_rcm_vs/RCM_ActionServer/feedback  measure rcm deviation msg.errors.p_trocar
 #   - /lbr/RCM_ActionServer/state               measure rcm tip msg.state.task.values
-mean_pairwise_distance_sub = rospy.Subscriber("visual_servo/mean_pairwise_distance", Float64, meanPairwiseDistanceCB)
+pairwise_distance_sub = rospy.Subscriber("visual_servo/pairwise_distance", Float64, meanPairwiseDistanceCB)
 twist_sub = rospy.Subscriber("visual_servo/twist", Twist, twistCB)
 h_rcm_vs_feedback_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/feedback", rcmActionFeedback, hRCMVSFeedbackCB)
 rcm_state_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/state", rcm, rCoMStateCB)
@@ -53,7 +54,7 @@ if __name__ == '__main__':
 
     rospy.loginfo("Saving measurements to {}".format(os.getcwd()))
 
-    mean_pairwise_distance_df.to_pickle("mean_pairwise_distance.pkl")
+    pairwise_distance_df.to_pickle("pairwise_distance.pkl")
     twist_df.to_pickle("twist.pkl")
     h_rcm_vs_feedback_df.to_pickle("h_rcm_vs_feedback.pkl")
     rcm_state_df = rcm_state_df.to_pickle("rcm_state.pkl")
