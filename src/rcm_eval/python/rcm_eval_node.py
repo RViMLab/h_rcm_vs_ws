9291d2f0aca2eff6e8ae3fc0c128d6dfc89e3ef2
mhubii <m.huber_1994@hotmail.de>
2021-05-25 14:16:02 +0100

    added time tracking

--- src/rcm_eval/python/rcm_eval_node.py
+++ src/rcm_eval/python/rcm_eval_node.py
@@ -9,46 +9,61 @@ from rcm_msgs.msg import rcm, rcmActionFeedback
 from h_vs.msg import pairwise_distance
 
 # dataframes to hold measurements
-pairwise_distance_df = pd.DataFrame(columns=["pairwise_distance"])
-twist_df = pd.DataFrame(columns=["Twist"])
-h_rcm_vs_feedback_df = pd.DataFrame(columns=["rcmFeedback"])
-rcm_state_df = pd.DataFrame(columns=["rcm"])
+pairwise_distance_df = pd.DataFrame(columns=["time", "pairwise_distance"])
+twist_df = pd.DataFrame(columns=["time", "Twist"])
+h_rcm_vs_feedback_df = pd.DataFrame(columns=["time", "rcmFeedback"])
+rcm_state_df = pd.DataFrame(columns=["time", "rcm"])
 
 # callbacks
-def meanPairwiseDistanceCB(msg: pairwise_distance):
+def pairwiseDistanceCB(msg: pairwise_distance):
     global pairwise_distance_df
-    pairwise_distance_df = pairwise_distance_df.append({"pairwise_distance": msg}, ignore_index=True)
+    pairwise_distance_df = pairwise_distance_df.append(
+        {
+            "time": rospy.Time.now().to_sec(), 
+            "pairwise_distance": msg
+        }, ignore_index=True
+    )
 
 def twistCB(msg: Twist):
     global twist_df
-    twist_df = twist_df.append({"Twist": msg}, ignore_index=True)
+    twist_df = twist_df.append(
+        {
+            "time": rospy.Time.now().to_sec(),
+            "Twist": msg
+        }, ignore_index=True
+    )
 
 def hRCMVSFeedbackCB(msg: rcmActionFeedback):
     global h_rcm_vs_feedback_df
-    h_rcm_vs_feedback_df = h_rcm_vs_feedback_df.append({"rcmFeedback": msg.feedback}, ignore_index=True)
+    h_rcm_vs_feedback_df = h_rcm_vs_feedback_df.append(
+        {
+            "time": rospy.Time.now().to_sec(),
+            "rcmFeedback": msg.feedback
+        }, ignore_index=True
+    )
 
 def rCoMStateCB(msg: rcm):
     global rcm_state_df
-    rcm_state_df = rcm_state_df.append({"rcm": msg}, ignore_index=True)
+    rcm_state_df = rcm_state_df.append(
+        {
+            "time": rospy.Time.now().to_sec(),
+            "rcm": msg
+        }, ignore_index=True
+    )
 
-# subscribe to
-#   - /lbr/visual_servo/pairwise_distance   measure visual error minimization
-#   - /lbr/visual_servo/twist                    measure error minimization
-#   - /lbr/h_rcm_vs/RCM_ActionServer/feedback  measure rcm deviation msg.errors.p_trocar
-#   - /lbr/RCM_ActionServer/state               measure rcm tip msg.state.task.values
-pairwise_distance_sub = rospy.Subscriber("visual_servo/pairwise_distance", Float64, meanPairwiseDistanceCB)
-twist_sub = rospy.Subscriber("visual_servo/twist", Twist, twistCB)
-h_rcm_vs_feedback_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/feedback", rcmActionFeedback, hRCMVSFeedbackCB)
-rcm_state_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/state", rcm, rCoMStateCB)
-
-# experiments
-#   - visual servo
-#     - random perturbation
-#     - random perturbation under rcm deviation
-#   - camera displacement
 
 if __name__ == '__main__':
     rospy.init_node("rcm_eval_node")
+
+    # subscribe to
+    #   - /lbr/visual_servo/pairwise_distance   measure visual error minimization
+    #   - /lbr/visual_servo/twist                    measure error minimization
+    #   - /lbr/h_rcm_vs/RCM_ActionServer/feedback  measure rcm deviation msg.errors.p_trocar
+    #   - /lbr/RCM_ActionServer/state               measure rcm tip msg.state.task.values
+    pairwise_distance_sub = rospy.Subscriber("visual_servo/pairwise_distance", pairwise_distance, pairwiseDistanceCB)
+    twist_sub = rospy.Subscriber("visual_servo/twist", Twist, twistCB)
+    h_rcm_vs_feedback_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/feedback", rcmActionFeedback, hRCMVSFeedbackCB)
+    rcm_state_sub = rospy.Subscriber("h_rcm_vs/RCM_ActionServer/state", rcm, rCoMStateCB)
 
     rospy.spin()
 
